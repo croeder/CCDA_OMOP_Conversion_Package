54a6b71eb34fa683f5e1b310e93fa241cdc7af08
Christophe Roeder <croeder@croeder.com>
2026-04-11 15:19:25 -0600

    started 2.0 development, release 2.0.1

--- bin/build_and_dist.sh
+++ bin/build_and_dist.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 # Use this script to build a new release and upload it to PyPI.
 
-VERSION="1.0.0"
+VERSION="2.0.1"
 echo "BUILDING $VERSION"
 sed s/X.Y.Z/$VERSION/ pyproject.toml.template > pyproject.toml
 python3 -m build
 
 if $(false) ; then
+#if $(true) ; then
   echo "UPLOADING $VERSION"
   python3 -m twine upload dist/ccda_to_omop-${VERSION}-py3-none-any.whl dist/ccda_to_omop-${VERSION}.tar.gz
 else
